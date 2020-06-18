9f83ffd48f87f5b2c3d8b7be668126c320f9db6f
Bluzzi <bluzzigame@gmail.com>
2020-06-19 00:20:13 +0200

    Application base

---
+++ Cape Changer/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace Cape_Changer
+{
+    /// <summary>
+    /// Logique d'interaction pour App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ Cape Changer/CapeChanger.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+
+namespace Cape_Changer
+{
+    /// <summary>
+    /// Logique d'interaction pour CapeChanger.xaml
+    /// </summary>
+    public partial class CapeChanger : Window
+    {
+        public CapeChanger()
+        {
+            InitializeComponent();
+        }
+
+        private void AddCape(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SelectCape(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
