da176c75c37df8a681205df2cc9b34d207098a9b
Bluzzi <bluzzigame@gmail.com>
2020-06-19 15:37:18 +0200

    CapeChanger (CS): create AddCapeInDisplayedList() method and others changes

--- Cape Changer/CapeChanger.xaml.cs
+++ Cape Changer/CapeChanger.xaml.cs
@@ -1,14 +1,15 @@
-﻿using Microsoft.Win32;
+﻿using Cape_Changer.Utils;
+using Microsoft.Win32;
+using System;
 using System.Diagnostics;
+using System.Drawing;
 using System.IO;
 using System.Threading;
 using System.Windows;
+using System.Windows.Media.Imaging;
 
 namespace Cape_Changer
 {
-    /// <summary>
-    /// Logique d'interaction pour CapeChanger.xaml
-    /// </summary>
     public partial class CapeChanger : Window
     {
         public string minecraftSkinPath;
@@ -16,21 +17,51 @@ namespace Cape_Changer
 
         public CapeChanger()
         {
-            minecraftSkinPath = FindSkinPackPath();
-            if (minecraftSkinPath == "") Process.GetCurrentProcess().Kill();
+            /*minecraftSkinPath = FindSkinPackPath();
+            if (minecraftSkinPath == "") Process.GetCurrentProcess().Kill();*/
 
             InitializeComponent();
+
+            foreach (string capePath in Directory.GetFiles(GetCapePath()))
+            {
+                AddCapeInDisplayedList(capePath);
+            }
+        }
+
+        private string GetCapePath()
+        {
+            string path = Directory.GetCurrentDirectory();
+
+            path = Path.Combine(path, @"capes\");
+
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+
+            return path;
         }
 
         private void AddCape(object sender, RoutedEventArgs e)
         {
-            string callbackPath = "";
+            // Verify if the cape format is good :
             OpenFileDialog fileBrowser = new OpenFileDialog();
+
             fileBrowser.Title = "Cape Texture";
             fileBrowser.Filter = "Image Files|*.jpg;*.jpeg;*.png;";
+            
             if (fileBrowser.ShowDialog() == true)
             {
-                callbackPath = fileBrowser.FileName;
+                // Copy the cape in cape list folder :
+                string capePath = fileBrowser.FileName;
+                string capeName = Path.GetFileName(capePath);
+
+                string finalPath = Path.Combine(GetCapePath(), @capeName);
+
+                File.Copy(capePath, finalPath);
+
+                // Add cape in the displayed list :
+                AddCapeInDisplayedList(finalPath);
             }
         }
 
@@ -53,6 +84,30 @@ namespace Cape_Changer
             restartMinecraft();
         }
 
+        /// <summary>
+        /// Add a cape in the interface.
+        /// </summary>
+        /// <param name="capePath">the cape path</param>
+        private void AddCapeInDisplayedList(string capePath)
+        {
+            System.Windows.Controls.Image cape = new System.Windows.Controls.Image();
+
+            BitmapImage bitmapImage = new BitmapImage(new Uri(capePath));
+            //Image image = ImageUtils.CropImage(Image.FromFile(capePath), new Rectangle(0, 0, 24, 18));
+
+            cape.Source = bitmapImage;
+            cape.Width = 500;
+            cape.Height = 200;
+            cape.Margin = new Thickness(15);
+
+            //cape.Source = ImageUtils.ImageToBitmapImage(ImageUtils.PixelateImage(image));
+            /*cape.Source = ImageUtils.ImageToBitmapImage(ImageUtils.Pixelate
+                (ImageUtils.ResizeImage(image, image.Width, image.Height),
+                new Rectangle(0, 0, image.Width, image.Height), (int)(image.Width * .25 + image.Height * .25) / 2));*/
+
+            CapesList.Children.Add(cape);
+        }
+
         private void restartMinecraft()
         {
             Process[] processes = Process.GetProcessesByName("Minecraft.Windows");
@@ -65,7 +120,7 @@ namespace Cape_Changer
             Process.Start("minecraft://");
         }
 
-        private string FindSkinPackPath()
+        /*private string FindSkinPackPath()
         {
             Process[] processes = Process.GetProcessesByName("Minecraft.Windows");
 
@@ -74,13 +129,14 @@ namespace Cape_Changer
                 string fileName = processes[0].MainModule.FileName;
                 int removePosition = fileName.Length - 21;
                 minecraftStartPath = fileName;
-                return fileName.Remove(removePosition) + @"data\skin_packs\vanilla";
+
+                return Path.Combine(fileName.Remove(removePosition), @"data\skin_packs\vanilla");
             }
             else
             {
                 MessageBox.Show("Start Minecraft, before Launching the application.");
                 return "";
             }
-        }
+        }*/
     }
 }
