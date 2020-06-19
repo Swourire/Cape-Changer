a031af22abe0fc9076ddbe6d14a90b5540ce55ce
Bluzzi <bluzzigame@gmail.com>
2020-06-19 15:36:17 +0200

    Create ImageUtils for images conversion

---
+++ Cape Changer/Utils/ImageUtils.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Windows.Media.Imaging;
+
+namespace Cape_Changer.Utils
+{
+    public class ImageUtils
+    {
+        public static Bitmap ResizeImage(Image image, int width, int height)
+        {
+            var destRect = new Rectangle(0, 0, width, height);
+            var destImage = new Bitmap(width, height);
+
+            destImage.SetResolution(image.HorizontalResolution, image.VerticalResolution);
+
+            using (var graphics = Graphics.FromImage(destImage))
+            {
+                graphics.CompositingMode = CompositingMode.SourceCopy;
+                graphics.CompositingQuality = CompositingQuality.HighQuality;
+                graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                graphics.SmoothingMode = SmoothingMode.HighQuality;
+                graphics.PixelOffsetMode = PixelOffsetMode.HighQuality;
+
+                using (var wrapMode = new ImageAttributes())
+                {
+                    wrapMode.SetWrapMode(WrapMode.TileFlipXY);
+                    graphics.DrawImage(image, destRect, 0, 0, image.Width, image.Height, GraphicsUnit.Pixel, wrapMode);
+                }
+            }
+
+            return destImage;
+        }
+
+        public static BitmapImage BitmapToBitmapImage(Bitmap bitmap)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                bitmap.Save(ms, System.Drawing.Imaging.ImageFormat.Jpeg);
+                System.Windows.Media.Imaging.BitmapImage bImg = new System.Windows.Media.Imaging.BitmapImage();
+
+                bImg.BeginInit();
+                bImg.StreamSource = new MemoryStream(ms.ToArray());
+                bImg.EndInit();
+
+                return bImg;
+            }
+        }
+
+        public static Image CropImage(Image img, Rectangle cropArea)
+        {
+            Bitmap bmpImage = new Bitmap(img);
+            return bmpImage.Clone(cropArea, bmpImage.PixelFormat);
+        }
+
+        public static BitmapImage ImageToBitmapImage(Image img)
+        {
+            using (var memory = new MemoryStream())
+            {
+                img.Save(memory, ImageFormat.Png);
+                memory.Position = 0;
+
+                var bitmapImage = new BitmapImage();
+                bitmapImage.BeginInit();
+                bitmapImage.StreamSource = memory;
+                bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapImage.EndInit();
+
+                return bitmapImage;
+            }
+        }
+
+        public static Bitmap Pixelate(Bitmap image, Rectangle rectangle, Int32 pixelateSize)
+        {
+            Bitmap pixelated = new System.Drawing.Bitmap(image.Width, image.Height);
+
+            // make an exact copy of the bitmap provided
+            using (Graphics graphics = System.Drawing.Graphics.FromImage(pixelated))
+                graphics.DrawImage(image, new System.Drawing.Rectangle(0, 0, image.Width, image.Height),
+                    new Rectangle(0, 0, image.Width, image.Height), GraphicsUnit.Pixel);
+
+            // look at every pixel in the rectangle while making sure we're within the image bounds
+            for (Int32 xx = rectangle.X; xx < rectangle.X + rectangle.Width && xx < image.Width; xx += pixelateSize)
+            {
+                for (Int32 yy = rectangle.Y; yy < rectangle.Y + rectangle.Height && yy < image.Height; yy += pixelateSize)
+                {
+                    Int32 offsetX = pixelateSize / 2;
+                    Int32 offsetY = pixelateSize / 2;
+
+                    // make sure that the offset is within the boundry of the image
+                    while (xx + offsetX >= image.Width) offsetX--;
+                    while (yy + offsetY >= image.Height) offsetY--;
+
+                    // get the pixel color in the center of the soon to be pixelated area
+                    Color pixel = pixelated.GetPixel(xx + offsetX, yy + offsetY);
+
+                    // for each pixel in the pixelate size, set it to the center color
+                    for (Int32 x = xx; x < xx + pixelateSize && x < image.Width; x++)
+                        for (Int32 y = yy; y < yy + pixelateSize && y < image.Height; y++)
+                            pixelated.SetPixel(x, y, pixel);
+                }
+            }
+
+            return pixelated;
+        }
+
+        public static Image PixelateImage(Image image)
+        {
+            var bitmap = new Bitmap(image.Width, image.Height);
+            using (var graphics = Graphics.FromImage(bitmap))
+            {
+                graphics.DrawImage(image, new Rectangle(0, 0, image.Width, image.Height),
+                    new Rectangle(0, 0, image.Width, image.Height), GraphicsUnit.Pixel);
+            }
+
+            // Loop through the image in 4x4 cells.
+            for (var yy = 0; yy < image.Height && yy < image.Height; yy += 2)
+            {
+                for (var xx = 0; xx < image.Width && xx < image.Width; xx += 2)
+                {
+                    var cellColors = new List<Color>();
+
+                    // Store each color from the 4x4 cell into cellColors.
+                    for (var y = yy; y < yy + 2 && y < image.Height; y++)
+                    {
+                        for (var x = xx; x < xx + 2 && x < image.Width; x++)
+                        {
+                            cellColors.Add(bitmap.GetPixel(x, y));
+                        }
+                    }
+
+                    // Get the average red, green, and blue values.
+                    var averageRed = cellColors.Aggregate(0, (current, color) => current + color.R) / cellColors.Count;
+                    var averageGreen = cellColors.Aggregate(0, (current, color) => current + color.G) / cellColors.Count;
+                    var averageBlue = cellColors.Aggregate(0, (current, color) => current + color.B) / cellColors.Count;
+
+                    var averageColor = Color.FromArgb(averageRed, averageGreen, averageBlue);
+
+                    // Go BACK over the 4x4 cell and set each pixel to the average color.
+                    for (var y = yy; y < yy + 2 && y < image.Height; y++)
+                    {
+                        for (var x = xx; x < xx + 2 && x < image.Width; x++)
+                        {
+                            bitmap.SetPixel(x, y, averageColor);
+                        }
+                    }
+                }
+            }
+            return bitmap;
+        }
+    }
+}
